9a1bbae3fca812f97228db78f55407e507e221ae
era <the@communist.dev>
2021-09-19 16:45:01 +0100

    simplify code

--- alarm_assert/alarm_assert/checker.py
+++ alarm_assert/alarm_assert/checker.py
@@ -21,24 +21,20 @@ class CheckerCase:
             raise FailedAssertion(message)
 
 class CheckerExecutor:
-    EXEC_TEMPLATE = '''
-    def assertion():
-        {assertion}
-    '''
-
+   
     def __init__(self, rule, alarm):
-        self.function = EXEC_TEMPLATE.format(assertion=rule)
+        self.rule = rule
         self.alarm = alarm
     
     def exec(self):
-        self.alarm.check(self.function)
+        self.alarm.check(self.rule)
 class Alarm:
     
     def alarm(self, exception):
         raise NotImplementedError
 
-    def check(self, function):
+    def check(self, rule):
         try:
-            function()
+            eval(rule)
         except FailedAssertion as e:
             self.alarm(e)
