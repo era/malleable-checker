8bd18fca319c421678c83dc90fb5a219b338fe0b
era <the@communist.dev>
2021-11-28 17:22:57 +0000

    formatting checker file

--- alarm_assert/alarm_assert/checker.py
+++ alarm_assert/alarm_assert/checker.py
@@ -1,5 +1,5 @@
 import pika
-from collections import namedtuple 
+from collections import namedtuple
 import json
 import os
 import sqlite3
@@ -7,8 +7,10 @@ import ast
 
 from configparser import ConfigParser
 
+
 class FailedAssertion(Exception):
     pass
+
 
 class CheckerCase:
 
@@ -20,13 +22,13 @@ class CheckerCase:
 
     def assertLessThan(self, collection, max_size, message):
         self.assertTrue(len(collection) < max_size, message)
-    
+
     def assertGreaterThan(self, collection, size, message):
         self.assertTrue(len(collection) > size, message)
 
     def assertFalse(self, expression, message):
         self.assertTrue(not expression, message)
-    
+
     def assertTrue(self, expression, message):
         if not expression:
             raise FailedAssertion(message)
@@ -43,13 +45,14 @@ class Dataset:
         and return an array with the results"""
         pass
 
+
 class SQLiteDataset(Dataset):
 
     def __init__(self, conn, id, sql):
         self.id = id
         self.sql = sql
         self.conn = conn
-    
+
     def identity(self):
         return self.id
 
@@ -57,13 +60,14 @@ class SQLiteDataset(Dataset):
         cur = self.conn.cursor()
         return cur.execute(self.sql).fetchall()
 
+
 class CheckerExecutor:
-   
+
     def __init__(self, rule, alarm, datasets):
         self.rule = rule
         self.alarm = alarm
         self.datasets = datasets
-    
+
     def exec(self):
         return self.alarm.check(self.rule, self.fetch_datasets())
 
@@ -73,8 +77,9 @@ class CheckerExecutor:
             data[dataset.identity()] = dataset.fetch()
         return data
 
+
 class Alarm:
-    
+
     def alarm(self, exception, datasets):
         raise NotImplementedError
 
@@ -84,15 +89,16 @@ class Alarm:
     def check(self, rule, datasets):
         try:
             parsed = ParseCheckerCode(rule).removed_unwanted_nodes()
-            
-            exec(compile(parsed, filename="", mode="exec"), {"datasets": datasets, 
-                        "FailedAssertion": FailedAssertion,
-                        "CheckerCase": CheckerCase})
+
+            exec(compile(parsed, filename="", mode="exec"), {"datasets": datasets,
+                                                             "FailedAssertion": FailedAssertion,
+                                                             "CheckerCase": CheckerCase})
             self.succeeds(rule, datasets)
             return True
         except Exception as e:
             self.alarm(e, datasets)
             return False
+
 
 class AlarmEventProducer(Alarm):
     """This Alarm implementation sends a message to {topic} when the alarm fails or succeeds.
@@ -110,7 +116,7 @@ class AlarmEventProducer(Alarm):
 
     SUCCEEDED_RESULT = 'succeeded'
     FAILED_RESULT = 'failed'
-    
+
     Event = namedtuple('Event', 'id result error rule dataset')
 
     def __init__(self, id, succeeded_topic, failed_topic, queue_connector, rule):
@@ -119,16 +125,17 @@ class AlarmEventProducer(Alarm):
         self.failed_topic = failed_topic
         self.queue_connector = queue_connector
         self.rule = rule
-        
+
         # Make sure the queues exist, otherwise messages will be dropped
         self.queue_connector.queue_declare(succeeded_topic)
         self.queue_connector.queue_declare(failed_topic)
 
     def succeeds(self, rule, datasets):
         self.emit(self.succeeded_topic, self.event(None, rule, datasets))
-    
+
     def alarm(self, exception, datasets):
-        self.emit(self.failed_topic, self.event(repr(exception), self.rule, datasets))
+        self.emit(self.failed_topic, self.event(
+            repr(exception), self.rule, datasets))
 
     def check(self, rule, datasets):
         return super().check(self.rule, datasets)
@@ -142,31 +149,35 @@ class AlarmEventProducer(Alarm):
     def emit(self, topic, event):
         self.queue_connector.publish(topic, json.dumps(event._asdict()))
 
+
 class RabbitMQConnector():
 
     ConnectionParams = namedtuple('ConnectionParams', 'host')
 
     def __init__(self, connection_params):
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(connection_params.host))
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(connection_params.host))
         self.channel = self.connection.channel()
-    
+
     def publish(self, topic, event):
-        self.channel.basic_publish(exchange='', routing_key=topic, body=json.dumps(event))
+        self.channel.basic_publish(
+            exchange='', routing_key=topic, body=json.dumps(event))
 
     def queue_declare(self, queue):
         self.channel.queue_declare(queue=queue)
+
 
 class ParseCheckerCode(ast.NodeTransformer):
 
     def __init__(self, code):
         self.tree = ast.parse(code)
-    
+
     def removed_unwanted_nodes(self):
         return self.visit(self.tree)
-    
+
     def handle_assigns(self, node):
-        ids = [target.id for target in node.targets] 
-        local_variables =  [key for key in globals().keys()]
+        ids = [target.id for target in node.targets]
+        local_variables = [key for key in globals().keys()]
         for id in ids:
             # If the user is trying to change the value of any globals()
             # they are probably doing something wrong
@@ -175,7 +186,7 @@ class ParseCheckerCode(ast.NodeTransformer):
         return node
 
     def handle_single_assigns(self, node):
-        local_variables =  [key for key in locals().keys()]
+        local_variables = [key for key in locals().keys()]
 
         # If the user is trying to change the value of any globals()
         # they are probably doing something wrong
@@ -186,10 +197,10 @@ class ParseCheckerCode(ast.NodeTransformer):
 
     def visit_FunctionDef(self, node):
         return None
-    
+
     def visit_AsyncFunctionDef(self, node):
         return None
-    
+
     def visit_ClassDef(self, node):
         return None
 
@@ -208,6 +219,7 @@ class ParseCheckerCode(ast.NodeTransformer):
     def visit_ImportFrom(self, node):
         return None
 
+
 # Execute this on a cronjob every 5 minutes
 if __name__ == '__main__':
 
@@ -215,7 +227,8 @@ if __name__ == '__main__':
 
     config_object.read(os.environ['CONFIG'])
 
-    rabbit_mq_env = RabbitMQConnector.ConnectionParams(config_object['CHECKER']['RABBITMQ_HOST'])
+    rabbit_mq_env = RabbitMQConnector.ConnectionParams(
+        config_object['CHECKER']['RABBITMQ_HOST'])
     rabbitmq_connector = RabbitMQConnector(rabbit_mq_env)
 
     sqlite_conn = sqlite3.connect(config_object['CHECKER']['SQLITE_PATH'])
@@ -227,19 +240,22 @@ if __name__ == '__main__':
     executors = []
 
     for checker in checkers:
-        alarm = AlarmEventProducer(checker[0], config_object['CHECKER']['SUCCEEDED_TOPIC'], config_object['CHECKER']['FAILED_TOPIC'], rabbitmq_connector, checker[1])
+        alarm = AlarmEventProducer(checker[0], config_object['CHECKER']['SUCCEEDED_TOPIC'],
+                                   config_object['CHECKER']['FAILED_TOPIC'], rabbitmq_connector, checker[1])
 
-        cur.execute("SELECT name, code FROM datasource, checker_datasource where checker_id = ? and datasource_id = id", [checker[0]])
+        cur.execute(
+            "SELECT name, code FROM datasource, checker_datasource where checker_id = ? and datasource_id = id", [checker[0]])
 
         datasources_code = cur.fetchall()
 
         datasources = []
 
         for ds_code in datasources_code:
-            datasources.append(SQLiteDataset(sqlite_conn, ds_code[0], ds_code[1]))
+            datasources.append(SQLiteDataset(
+                sqlite_conn, ds_code[0], ds_code[1]))
 
         executors.append(CheckerExecutor(checker[1], alarm, datasources))
-        
+
     for executor in executors:
         checker_id = executor.alarm.id
         status = 'GREEN'
@@ -249,10 +265,12 @@ if __name__ == '__main__':
             status = 'GREEN'
         else:
             status = 'RED'
-            
-        cur.execute('UPDATE checker set status = ? where id = ?', [status, checker_id])
-        cur.execute("INSERT INTO CHECKER_EXECUTION (checker_id, run_at, status) VALUES(?, strftime('%Y-%m-%d %H:%M:%S', 'now'), ?)", [checker_id, status])
-    
+
+        cur.execute('UPDATE checker set status = ? where id = ?',
+                    [status, checker_id])
+        cur.execute(
+            "INSERT INTO CHECKER_EXECUTION (checker_id, run_at, status) VALUES(?, strftime('%Y-%m-%d %H:%M:%S', 'now'), ?)", [checker_id, status])
+
     sqlite_conn.commit()
     sqlite_conn.close()
-    
+
--- alarm_assert/tests/checker_test.py
+++ alarm_assert/tests/checker_test.py
@@ -4,26 +4,29 @@ from unittest.mock import ANY
 
 import alarm_assert.checker as checker
 
+
 class FakeAlarm(checker.Alarm):
     def alarm(self, exception, _):
         raise exception
-    
+
     def succeeds(self, _rule, _datasets):
         pass
+
 
 class Dataset(checker.Dataset):
     def __init__(self, name, data):
         self.name = name
         self.data = data
-    
+
     def identity(self):
         return self.name
-    
+
     def fetch(self):
         return self.data
 
+
 class TestAlarm(unittest.TestCase):
-    
+
     def test_if_alarm_triggers(self):
         rule = "CheckerCase().assertTrue(False, 'Something bad happened')"
         with self.assertRaises(checker.FailedAssertion):
@@ -33,10 +36,10 @@ class TestAlarm(unittest.TestCase):
         rule = "CheckerCase().assertTrue(True, 'Something bad happened')"
         FakeAlarm().check(rule, [])
 
-
     def test_can_use_datasets_in_rules(self):
         rule = "CheckerCase().assertGreaterThan(datasets['my_data'], 2, 'Something bad happened')"
-        FakeAlarm().check(rule, {'my_data': [1,2,3]})
+        FakeAlarm().check(rule, {'my_data': [1, 2, 3]})
+
 
 class TestCheckerExecutor(unittest.TestCase):
 
@@ -50,11 +53,12 @@ class TestCheckerExecutor(unittest.TestCase):
     def test_if_builds_datasets(self):
         a = Dataset('my_cool_data', ['123'])
         b = not_empty = Dataset('b', ['1234'])
-        exec = checker.CheckerExecutor(None, FakeAlarm(), [a,b])
+        exec = checker.CheckerExecutor(None, FakeAlarm(), [a, b])
         datasets = exec.fetch_datasets()
 
         self.assertEqual({a.identity(): a.fetch(),
                           b.identity(): b.fetch()}, datasets)
+
 
 class TestAlarmEventProducer(unittest.TestCase):
 
@@ -66,24 +70,24 @@ class TestAlarmEventProducer(unittest.TestCase):
         return checker.AlarmEventProducer(self.ALARM_ID, self.SUCCEEDED_TOPIC, self.FAILURE_TOPIC, mock_connector, rule)
 
     def test_if_emit_failure_event(self):
-         rule = "CheckerCase().assertTrue(False, 'Something bad happened')"
-         connector = Mock()
-         self.alarm(connector, rule).check(None, [])
-         connector.publish.assert_called_with(self.FAILURE_TOPIC, ANY)
+        rule = "CheckerCase().assertTrue(False, 'Something bad happened')"
+        connector = Mock()
+        self.alarm(connector, rule).check(None, [])
+        connector.publish.assert_called_with(self.FAILURE_TOPIC, ANY)
 
     def test_if_emit_succeeded_event(self):
-         rule = "CheckerCase().assertTrue(True, 'Something bad happened')"
-         connector = Mock()
-         self.alarm(connector, rule).check(None, [])
-         connector.publish.assert_called_with(self.SUCCEEDED_TOPIC, ANY)
-        
+        rule = "CheckerCase().assertTrue(True, 'Something bad happened')"
+        connector = Mock()
+        self.alarm(connector, rule).check(None, [])
+        connector.publish.assert_called_with(self.SUCCEEDED_TOPIC, ANY)
 
     def test_builds_correctly_succeeded_event(self):
         event = self.alarm(Mock(), 'MY_RULE').event(None, 'MY_RULE')
         self.assertEqual(TestAlarmEventProducer.ALARM_ID, event.id)
         self.assertEqual('MY_RULE', event.rule)
         self.assertEqual(None, event.error)
-        self.assertEqual(checker.AlarmEventProducer.SUCCEEDED_RESULT, event.result)
+        self.assertEqual(
+            checker.AlarmEventProducer.SUCCEEDED_RESULT, event.result)
 
     def test_builds_correctly_failed_event(self):
         exception = Exception('test')
@@ -91,4 +95,6 @@ class TestAlarmEventProducer(unittest.TestCase):
         self.assertEqual(TestAlarmEventProducer.ALARM_ID, event.id)
         self.assertEqual('MY_RULE', event.rule)
         self.assertEqual(exception, event.error)
-        self.assertEqual(checker.AlarmEventProducer.FAILED_RESULT, event.result)
+        self.assertEqual(
+            checker.AlarmEventProducer.FAILED_RESULT, event.result)
+
